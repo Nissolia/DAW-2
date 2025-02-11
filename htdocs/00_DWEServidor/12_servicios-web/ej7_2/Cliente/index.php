eea4ab13f72052e1687acd39e656ffc4c5005420
Noelia\Portatil_Noelia <nissoliadesing@gmail.com>
2025-02-11 11:29:39 +0100

    dwes - ejercicio baraja completado

--- htdocs/00_DWEServidor/12_servicios-web/ej7_2/Cliente/index.php
+++ htdocs/00_DWEServidor/12_servicios-web/ej7_2/Cliente/index.php
@@ -9,7 +9,7 @@ if (session_status() === PHP_SESSION_NONE) session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Baraja</title>
     <style>
-        img{
+        img {
             width: 60px;
         }
     </style>
@@ -27,35 +27,39 @@ en la URL es menor que 1 o mayor que 40, se debe devolver un error. -->
         <input type="submit" value="Crear baraja">
     </form>
 
-<?php
+    <?php
 
-if (isset($_REQUEST['numero'])) {
-    $parametros =  '?numero=' . $_REQUEST['numero'];
-    $data = @file_get_contents("http://localhost//00_DWEServidor/12_servicios-web/ej7_2/Servidor/crearBaraja.php" . $parametros);
-    mostrarDatos(json_decode($data));
-}
-
-function mostrarEstado($cartas)
-{
-    echo "STATUS: " . $cartas->codEstado;
-    echo "<br>" . $cartas->mensaje;
-}
-function mostrarDatos($cartas)
-{
-    if ($cartas->codEstado == 200) {
-        echo "<hr style='margin-top:20px'>";
-        echo "<table style='text-align:center; padding:10px;'><tr><th style='padding:10px'>Número</th><th style='padding:10px'>Palo</th></tr>";
-        // Accedemos a la propiedad "baraja"
-        foreach ($cartas->baraja as $carta) {
-            echo "<tr><td style=' border-bottom:1px solid black; border collapse:collapse;'>" . $carta->numero  . "</td>";
-            echo "<td style=' border-bottom:1px solid black; border collapse:collapse;'>" . $carta->palo . "</td></tr>";
+    if (isset($_REQUEST['numero'])) {
+        $parametros =  '?numero=' . $_REQUEST['numero'];
+        $conversion = @file_get_contents("http://localhost//00_DWEServidor/12_servicios-web/ej7_2/Servidor/crearBaraja.php" . $parametros);
+        $respuesta = json_decode($conversion);
+        echo "<br> Cabecera: " . $http_response_header[0] . "<br>";
+        if ($http_response_header[0] == "HTTP/1.1 200 OK") {
+            mostrarDatos($respuesta);
         }
-        echo "</table>";
-    } else {
-        mostrarEstado($cartas);
     }
-}
-?>
+
+    function mostrarEstado($cartas)
+    {
+        echo "STATUS: " . $cartas->codEstado;
+        echo "<br>" . $cartas->mensaje;
+    }
+    function mostrarDatos($cartas)
+    {
+        if ($cartas->codEstado == 200) {
+            echo "<hr style='margin-top:20px'>";
+            echo "<table style='text-align:center; padding:10px;'><tr><th style='padding:10px'>Número</th><th style='padding:10px'>Palo</th></tr>";
+            // Accedemos a la propiedad "baraja"
+            foreach ($cartas->baraja as $carta) {
+                echo "<tr><td style=' border-bottom:1px solid black; border collapse:collapse;'>" . $carta->numero  . "</td>";
+                echo "<td style=' border-bottom:1px solid black; border collapse:collapse;'>" . $carta->palo . "</td></tr>";
+            }
+            echo "</table>";
+        } else {
+            mostrarEstado($cartas);
+        }
+    }
+    ?>
 </body>
 
 
