dfd45c1295a4805b10fdd5919f41b4b89e17c3d1
DESKTOP-NQO1TN8\Noelia <nissoliadesing@gmail.com>
2025-02-14 21:13:21 +0100

    dwes - framework - inicio de ejercicios

--- Frameworks-Js/Angular/proyecto001/src/app/app.component.ts
+++ Frameworks-Js/Angular/proyecto001/src/app/app.component.ts
@@ -4,17 +4,39 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  // podemos crear variables
   title = 'proyecto001';
-  nombre = "Noelia";
+  nombre = 'Noelia';
   edad = 21;
   fumador = false;
-  sueldos = [1233,1200,1900];
-  apellido : string = 'Baños';
+  sueldos = [1233, 1200, 1900];
+  apellido: string = 'Baños';
+  //literal de objeto
+  provincias = [
+    { id: 1, nombre: 'Huelva' },
+    { id: 2, nombre: 'Sevilla' },
+    { id: 3, nombre: 'Cádiz' },
+    { id: 4, nombre: 'Málaga' },
+    { id: 5, nombre: 'Córdoba' },
+    { id: 6, nombre: 'Almería' },
+  ];
 
-  esFumador(){
-    return this.fumador?"Esta persona fuma.":"Esta persona no fuma";
+  contador = 0;
+  // se pueden hacer también métodos
+  esFumador() {
+    return this.fumador ? 'Esta persona fuma.' : 'Esta persona no fuma';
+  }
+  diHola() {
+    alert('¡Hola!');
+  }
+  // funciones para el contador
+  incrementar() {
+    this.contador++;
+  }
+  decrementar() {
+    this.contador--;
   }
 }
