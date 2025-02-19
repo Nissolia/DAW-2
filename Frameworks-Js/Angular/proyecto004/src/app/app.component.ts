c64e57f90eb0a56533b29e617a9b15bc47839e75
DESKTOP-NQO1TN8\Noelia <nissoliadesing@gmail.com>
2025-02-19 21:42:18 +0100

    dwec - cambios en el proyecto004 - creacion de dado + uso en la carpeta componente padre

--- Frameworks-Js/Angular/proyecto004/src/app/app.component.ts
+++ Frameworks-Js/Angular/proyecto004/src/app/app.component.ts
@@ -4,8 +4,33 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'proyecto004';
+  valor1!: number;
+  valor2!: number;
+  valor3!: number;
+  resultado = '';
+
+  constructor() {
+    this.lanzarDados();
+  }
+
+  lanzarDados() {
+    this.valor1 = this.generaAleatorio();
+    this.valor2 = this.generaAleatorio();
+    this.valor3 = this.generaAleatorio();
+    this.comprobarResultado();
+  }
+  comprobarResultado() {
+    if (this.valor1 == this.valor2 && this.valor2 == this.valor3) {
+      this.resultado = '¡Has ganado!';
+    } else {
+      this.resultado = '-';
+    }
+  }
+
+  generaAleatorio() {
+    return Math.floor(Math.random() * 6) + 1;
+  }
 }
