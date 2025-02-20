16da838427c544c9d6d06caddeb26ff559b72080
Noelia\Portatil_Noelia <nissoliadesing@gmail.com>
2025-02-20 22:16:09 +0100

    dwes - laravel, probando apartados de laravel en web.php y tomando los controladores y los views

---
+++ htdocs/00_DWEServidor/13_Laravel/nombreProyecto/resources/views/secundaria.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Secundaria</title>
+</head>
+<body>
+<h1>Bienvenido a la página secundaria</h1>
+<a href="/">Volver al index</a>
+
+</body>
+</html>
--- htdocs/00_DWEServidor/13_Laravel/nombreProyecto/routes/web.php
+++ htdocs/00_DWEServidor/13_Laravel/nombreProyecto/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\secundarioController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('index');
+// });
+Route::get('/', IndexController::class);
+
+Route::get('/secundaria', secundarioController::class);
